a1ad8a5e747a4e0511b8cadd6cdd1cfad5ba8345
Kyle Corbitt <kyle@corbt.com>
2015-12-18 12:36:46 +0000

    update discord to latest version

--- Casks/discord.rb
+++ Casks/discord.rb
@@ -1,5 +1,5 @@
 cask 'discord' do
-  version '0.0.231'
+  version '0.0.233'
   sha256 '78da63758a567c3c12ff5e138b4613096119f66adb5fce6fb530bb50891c1374'
 
   url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg"
